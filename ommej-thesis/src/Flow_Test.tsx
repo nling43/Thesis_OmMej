b9b46bd488647beaf9979160df907395e0c952af
Quang M Nguyen <quangteoqs15@gmail.com>
2023-02-06 16:31:36 +0100

    Basic stuff React FLow

--- ommej-thesis/src/Flow_Test.tsx
+++ ommej-thesis/src/Flow_Test.tsx
@@ -1,6 +1,15 @@
-import ReactFlow, { Controls, Background } from 'reactflow';
+import ReactFlow, { 
+  Controls, 
+  Background, 
+  applyNodeChanges, 
+  applyEdgeChanges,
+  addEdge,
+  Connection,
+  Edge,
+} from 'reactflow';
 import 'reactflow/dist/style.css';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback} from 'react';
+
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
   return {
@@ -26,9 +35,11 @@ function useWindowDimensions() {
 
 
 
-const edges = [{ id: '1-2', source: '1', target: '2' }];
+const start_edges = [{ id: '1-2', source: '1', target: '2' }];
 
-const nodes = [
+const initialEdges: any[] | (() => any[]) = [];
+
+const start_nodes = [
   {
     id: '1',
     data: { label: 'Hello' },
@@ -44,9 +55,30 @@ const nodes = [
 
 function Flow() {
   const { height, width } = useWindowDimensions();
+  const [nodes, setNodes] = useState(start_nodes);
+  const [edges, setEdges] = useState(initialEdges);
+
+  const onNodesChange = useCallback(
+    (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    []
+  );
+
+  const onEdgesChange = useCallback(
+    (changes: any) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    []
+  );
+
+  const onConnect = useCallback((params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)), []);
+
   return (
     <div style={{ height: height-20,  width: width-20 }}>
-      <ReactFlow nodes={nodes} edges={edges}>
+      <ReactFlow 
+      nodes={nodes} 
+      onNodesChange={onNodesChange}
+      edges={edges}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      >
         <Background />
         <Controls />
       </ReactFlow>
