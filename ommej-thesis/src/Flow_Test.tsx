4d4a2586686f2bff275288fba864cc4fd96f5dc6
Quang M Nguyen <quangteoqs15@gmail.com>
2023-02-09 13:42:12 +0100

    MiniMap

--- ommej-thesis/src/Flow_Test.tsx
+++ ommej-thesis/src/Flow_Test.tsx
@@ -5,8 +5,12 @@ import ReactFlow, {
   applyEdgeChanges,
   addEdge,
   Connection,
+  Node,
   Edge,
   Panel,
+  MiniMap,
+  NodeChange,
+  EdgeChange,
 } from 'reactflow';
 import 'reactflow/dist/style.css';
 import { useState, useEffect, useCallback} from 'react';
@@ -34,39 +38,52 @@ function useWindowDimensions() {
   return windowDimensions;
 }
 
+const nodeColor = (node: Node) => {
+  switch (node.type) {
+    case 'question':
+      return 'red';
+    case 'answer':
+      return 'green';
+    default:
+      return 'blue';
+  }
+}
 
+const start_edges:Edge[]= [{ id: '1-2', source: '1', target: '2' }];
 
-const start_edges = [{ id: '1-2', source: '1', target: '2' }];
+const initialEdges: Edge[] | (() => any[]) = [];
 
-const initialEdges: any[] | (() => any[]) = [];
-
-const start_nodes = [
+const start_nodes:Node[] = [
   {
-    id: '1',
+    id: "1",
     data: { label: 'Hello' },
     position: { x: 0, y: 0 },
-    type: 'input',
+    type: 'question',
+    style: { background: 'red' ,color: 'white'},
+  
   },
   {
     id: '2',
     data: { label: 'World' },
     position: { x: 100, y: 100 },
+    type: 'answer',
+    style: { background: 'green', color: 'white'},
   },
 ];
 
 function Flow() {
   const { height, width } = useWindowDimensions();
-  const [nodes, setNodes] = useState(start_nodes);
-  const [edges, setEdges] = useState(initialEdges);
+  const [nodes, setNodes] = useState<Node[]>(start_nodes);
+  const [edges, setEdges] = useState<Edge[]>(initialEdges);
 
   const onNodesChange = useCallback(
-    (changes: any) => setNodes((nds) => applyNodeChanges(changes, nds)),
-    []
+    (changes: NodeChange[]) => setNodes((nds) => applyNodeChanges(changes, nds)),
+    [setNodes]
   );
 
   const onEdgesChange = useCallback(
-    (changes: any) => setEdges((eds) => applyEdgeChanges(changes, eds)),
-    []
+    (changes: EdgeChange[]) => setEdges((eds) => applyEdgeChanges(changes, eds)),
+    [setEdges]
   );
 
   const onConnect = useCallback((params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)), []);
@@ -74,14 +91,18 @@ function Flow() {
   return (
     <div style={{ height: height-20,  width: width-20 }}>
       <ReactFlow 
+      deleteKeyCode={null}
       nodes={nodes} 
       onNodesChange={onNodesChange}
       edges={edges}
       onEdgesChange={onEdgesChange}
       onConnect={onConnect}
+      
       >
+        
         <Background />
         <Controls />
+        <MiniMap nodeColor={nodeColor} nodeStrokeWidth={3} zoomable pannable/>
       </ReactFlow>
     </div>
   );
