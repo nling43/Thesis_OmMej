9375e8a0265e80f3dd6f49524b7767825a89c082
Quang M Nguyen <quangteoqs15@gmail.com>
2023-02-17 19:13:19 +0100

    Zustand test and reorganize the file

---
+++ ommej-thesis/src/App/index.tsx
@@ -0,0 +1,14 @@
+import Flow from '../Store/Flow_Zustand';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">React Flow - CRA Example</header>
+      <Flow />
+    </div>
+  );
+}
+
+export default App;
---
+++ ommej-thesis/src/Flow/CustomNode.tsx
@@ -0,0 +1,46 @@
+import { memo, FC, CSSProperties } from 'react';
+import { Handle, Position, NodeProps } from 'reactflow';
+import { NodeResizer } from '@reactflow/node-resizer';
+
+import '@reactflow/node-resizer/dist/style.css';
+
+const sourceHandleStyleA: CSSProperties = { left: 50 };
+const sourceHandleStyleB: CSSProperties = {
+  right: 50,
+  left: 'auto',
+};
+
+const CustomNode: FC<NodeProps> = ({ data, xPos, yPos }) => {
+  return (
+    <>
+      <NodeResizer />
+      <Handle type="target" position={Position.Top} />
+      <div>
+        <div>
+          Label: <strong>{data.label}</strong>
+        </div>
+        <div>
+          Position:{' '}
+          <strong>
+            {xPos.toFixed(2)},{yPos.toFixed(2)}
+          </strong>
+        </div>
+      </div>
+
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="a"
+        style={sourceHandleStyleA}
+      />
+      <Handle
+        type="source"
+        position={Position.Bottom}
+        id="b"
+        style={sourceHandleStyleB}
+      />
+    </>
+  );
+};
+
+export default memo(CustomNode);
