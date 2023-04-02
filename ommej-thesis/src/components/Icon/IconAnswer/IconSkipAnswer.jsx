c54ac4578a97c6a6d0678b726fe2d5cebe60900f
Quang M Nguyen <81567595+bepp-boop@users.noreply.github.com>
2023-04-02 14:55:57 +0200

    Icon for the tree

--- ommej-thesis/src/components/Icon/IconAnswer/IconSkipAnswer.jsx
+++ ommej-thesis/src/components/Icon/IconAnswer/IconSkipAnswer.jsx
@@ -1,13 +1,22 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown,faCircle } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDown, faCircle } from "@fortawesome/free-solid-svg-icons";
 
-function IconText() {
-  return (
-    <span className="fa-layers fa-fw fa-lg">
+function IconText({ zoomIn }) {
+  if (zoomIn) {
+    return (
+      <span className="fa-layers fa-fw fa-lg">
+        <FontAwesomeIcon icon={faCircle} transform="grow-50" />
+        <FontAwesomeIcon icon={faArrowDown} transform="grow-30" inverse />
+      </span>
+    );
+  } else {
+    return (
+      <span className="fa-layers fa-fw fa-lg">
         <FontAwesomeIcon icon={faCircle} transform="grow-10"/>
-        <FontAwesomeIcon icon={faArrowDown} transform="shrink-4" inverse />   
-    </span>
-    )
+        <FontAwesomeIcon icon={faArrowDown} transform="shrink-4" inverse />
+      </span>
+    );
+  }
 }
 
-export default IconText
+export default IconText;
--- ommej-thesis/src/components/nodes/Answers/AnswerNodeAccommodations.jsx
+++ ommej-thesis/src/components/nodes/Answers/AnswerNodeAccommodations.jsx
@@ -1,5 +1,5 @@
 import React, { memo } from "react";
-import { Handle } from "reactflow";
+import { Handle,useStore } from "reactflow";
 import styled from "styled-components";
 import IconAccommodationAnswer from "../../Icon/IconAnswer/IconAccommodationAnswer";
 
@@ -26,7 +26,34 @@ const Node = styled.div`
   }
 `;
 
+const NodeZoomed = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+  width: 100px;
+  border-radius: 15px;
+
+  background: ${(props) => props.theme.answerBg};
+  color: ${(props) => props.theme.questionTextSingleAccommodation};
+  border: 1px solid
+    ${(props) =>
+      props.selected ? props.theme.primary : props.theme.nodeBorder};
+
+  .react-flow__handle {
+    background: ${(props) => props.theme.handleInputAnswerColor};
+    width: 8px;
+    height: 10px;
+    border: 0px solid #000;
+    border-radius: 3px;
+  }
+`;
+
+const zoomSelector = (s) => s.transform[2] >= 0.5;
+
 export default memo(({ data, selected }) => {
+  const showContent = useStore(zoomSelector);
+  if (showContent) {
   return (
     <Node selected={selected}>
       <Handle type="target" position="top" />
@@ -36,4 +63,16 @@ export default memo(({ data, selected }) => {
       <Handle type="source" position="bottom" id="1"></Handle>
     </Node>
   );
+}
+  else{
+  return (
+    <NodeZoomed selected={selected}>
+      <Handle type="target" position="top" />
+      <div>
+        <IconAccommodationAnswer zoomIn/>
+      </div>
+      <Handle type="source" position="bottom" id="1"></Handle>
+    </NodeZoomed>
+  );
+  }
 });
