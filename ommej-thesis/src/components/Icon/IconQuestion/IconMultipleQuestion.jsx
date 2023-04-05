3f30c0f1321439785ddd3e6ea15619c1a92cd270
Niklas Ling <72392907+nling43@users.noreply.github.com>
2023-04-05 16:00:24 +0200

    icon fix

--- ommej-thesis/src/components/Icon/IconQuestion/IconMultipleQuestion.jsx
+++ ommej-thesis/src/components/Icon/IconQuestion/IconMultipleQuestion.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { faListCheck, faSquare } from '@fortawesome/free-solid-svg-icons'
-function IconMultipleQuestion({zoomIn}) {
-  if(zoomIn){
-  return   (
-    <span className="fa-layers fa-fw fa-lg">
-        <FontAwesomeIcon icon={faSquare} transform="grow-80"/>
-        <FontAwesomeIcon icon={faListCheck} transform="grow-40" color='black' />
-    </span>
-  )
-}else{
-  return (
-    <span className="fa-layers fa-fw fa-lg">
-        <FontAwesomeIcon icon={faSquare} transform="grow-30"/>
-        <FontAwesomeIcon icon={faListCheck} transform="grow-7" color='black' />
-    </span>
-  )
-}}
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faListCheck, faSquare } from "@fortawesome/free-solid-svg-icons";
+function IconMultipleQuestion({ zoomIn }) {
+	if (zoomIn) {
+		return (
+			<span className="fa-layers fa-fw fa-lg">
+				<FontAwesomeIcon icon={faSquare} transform="grow-80" />
+				<FontAwesomeIcon icon={faListCheck} transform="grow-40" color="black" />
+			</span>
+		);
+	} else {
+		return (
+			<span className="fa-layers fa-fw fa-lg">
+				<FontAwesomeIcon icon={faSquare} transform="grow-30" />
+				<FontAwesomeIcon icon={faListCheck} transform="grow-7" color="black" />
+			</span>
+		);
+	}
+}
 
-export default IconMultipleQuestion
+export default IconMultipleQuestion;
